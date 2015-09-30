833efe1930e372e52438468ba285a1c8d7131d95
Brian Newsom <brian.newsom@colorado.edu>
2015-09-30 02:40:22 -0600

    Chrome extension now handles multiple tabs start stopping, stays off in new tab

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -2,13 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var PLAYING = true;
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
 	console.log('Making music happen!');
+	PLAYING = !PLAYING;
 
-  chrome.tabs.sendMessage(tab.id, {data: "toggle"}, function(response) {
-		console.log('sent toggle');
-		console.log(response.data);
-  });
+	var message = {data: "toggle", playing: PLAYING};
+	chrome.tabs.sendMessage(tab.id, message);
+	/*
+	chrome.tabs.query({}, function(tabs) {
+			var message = {data: "toggle", playing: PLAYING};
+			for (var i=0; i < tabs.length; ++i) {
+					chrome.tabs.sendMessage(tabs[i].id, message);
+			}
+	});
+	*/
+
 });
+
+// Receive playing query
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.playing == "query")
+      sendResponse({playing: PLAYING});
+  });
--- chrome_ext/content.js
+++ chrome_ext/content.js
@@ -1,20 +1,42 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('moosik.min.js');
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head||document.documentElement).appendChild(s);
+var INJECTED = false;
+// Is this individual tab playing right now?
+var tabIsPlaying = false;
 
-console.log('makin money');
+// Check if globally playing - only inject if so
+chrome.runtime.sendMessage({playing: "query"}, function(response) {
+	if (response.playing){
+		console.log('injecting moosik');
+		injectMoosik();
+		tabIsPlaying = true;
+	}
+});
+
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.data == "toggle"){
+    if (request.data == "toggle" && request.playing === false ){
+			console.log(request);
+			// Currently playing, ensure it is disabled
 			toggle();
-      sendResponse({data: "toggled"});
+			sendResponse({data: "toggled"});
+		} else if (request.data == "toggle" && request.playing === true){
+			// Not playing, only toggle if it is __
+			if (INJECTED) toggle();
+			else injectMoosik();
+			sendResponse({data: "toggled"});
 		}
   }
 );
+
+function injectMoosik() {
+	var s = document.createElement('script');
+	s.src = chrome.extension.getURL('moosik.min.js');
+	s.onload = function() {
+			this.parentNode.removeChild(this);
+	};
+	(document.head||document.documentElement).appendChild(s);
+	INJECTED = true;
+}
 
 function toggle() {
 	var s = document.createElement('script');
@@ -23,4 +45,5 @@ function toggle() {
 			this.parentNode.removeChild(this);
 	};
 	(document.head||document.documentElement).appendChild(s);
+	tabIsPlaying = !tabIsPlaying;
 }
