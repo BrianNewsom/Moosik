054f821154a11d9db8cc9e170f419750658608c7
Brian Newsom <brian.newsom@colorado.edu>
2015-10-10 12:45:24 -0600

    Set delay before play - set max tempo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,79 +1,87 @@
 function MoosikMain() {
+
 	'use strict';
-
 	var main = this;
-	this.playing = false;
+	var delayTime = 2000; // in ms
+	
+	// Delay execution to avoid stutter
+	setTimeout(function(){	
 
-	var constants = new Constants();
-	var songAPI = new SongAPI(constants);
+		main.playing = false;
 
-	var handler = new Tautologistics.NodeHtmlParser.HtmlBuilder(function (error, dom) {
-		if (error)
-			console.log(error)
-		else
-			console.log('successfully parsed')
-	});
+		var constants = new Constants();
+		var songAPI = new SongAPI(constants);
 
-	var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
-	parser.parseComplete(document.body.innerHTML);
+		var handler = new Tautologistics.NodeHtmlParser.HtmlBuilder(function (error, dom) {
+			if (error)
+				console.log(error)
+				return;
+		});
 
-	var parsedHTML = handler.dom;
+		var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
+		parser.parseComplete(document.body.innerHTML);
 
-	var TREE = [];
+		var parsedHTML = handler.dom;
 
-	parse(parsedHTML, TREE)
+		var TREE = [];
 
-	var scale= songAPI.buildScale(TREE[1])
+		parse(parsedHTML, TREE)
 
-	songAPI.buildDrums(TREE[0], function(drumSeqShort) {
-		var drumSeq = drumSeqShort.wrapExtend(128);
+		var scale= songAPI.buildScale(TREE[1])
 
-		var seqArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+		songAPI.buildDrums(TREE[0], function(drumSeqShort) {
+			var drumSeq = drumSeqShort.wrapExtend(128);
 
-		var pat3 = songAPI.buildPluck(TREE[2]);
-		var pluckSeq = pat3.wrapExtend(128);
-		
+			var seqArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-		var tempo = 'BPM' + (60 + TREE[0].length)
-		var countsPerMeasure = 16
-		var interval = tempo + " L" + countsPerMeasure
+			var pat3 = songAPI.buildPluck(TREE[2]);
+			var pluckSeq = pat3.wrapExtend(128);
+			
 
-		var j = 0 
+			
+			console.log(TREE[0].length);
+			var tempo = Math.min(140, 60 + TREE[0].length);
+			var tempoText = 'BPM' + tempo ;
+			var countsPerMeasure = 16;
+			var interval = tempoText + " L" + countsPerMeasure;
 
-		var instruments = new Instruments();
-		var synth = instruments.synth;
-		var lead = instruments.lead;
-		var loop = new MainLoop(interval, songAPI, scale, drumSeq, seqArray, pluckSeq, lead);
+			var j = 0;
 
-		var control = new Control(loop, synth);
-		
-		// Start loop initially
-		control.start();
-		main.playing = true;
+			var instruments = new Instruments();
+			var synth = instruments.synth;
+			var lead = instruments.lead;
+			var loop = new MainLoop(interval, songAPI, scale, drumSeq, seqArray, pluckSeq, lead);
 
-		main.start = function() {
-			// Global start & (accesses control)
-			if (!control){
-				var control = new Control(loop, synth);	
-			}
+			var control = new Control(loop, synth);
+			
+			// Start loop initially
 			control.start();
 			main.playing = true;
-		}
-		
-		main.stop = function() {
-			// Global stop (accesses control)
-			control.stop();
-			main.playing = false;
-		}
-		
-		main.toggle = function() {
-			if (main.playing) {
-				main.stop();
-			} else {
-				main.start();
+
+			main.start = function() {
+				// Global start & (accesses control)
+				if (!control){
+					var control = new Control(loop, synth);	
+				}
+				control.start();
+				main.playing = true;
 			}
-		}
-	});
+			
+			main.stop = function() {
+				// Global stop (accesses control)
+				control.stop();
+				main.playing = false;
+			}
+			
+			main.toggle = function() {
+				if (main.playing) {
+					main.stop();
+				} else {
+					main.start();
+				}
+			}
+		});
+	}, delayTime);
 }
 
 // We need an object to start and stop from the chrome extension
